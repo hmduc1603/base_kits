b982173f94b5a499bd73ee7d35fea46de6ca28f7
hmduc1603 <hmduc1603@gmail.com>
2023-04-03 21:59:40 +0700

    Update

--- lib/src/admob/admob_event_listener.dart
+++
@@ -1,8 +0,0 @@
-import 'package:easy_ads_flutter/easy_ads_flutter.dart';
-
-mixin AdmobEventListener {
-  onOpenAdEvent(AdEventType adEventType);
-  onInterstitialAdEvent(AdEventType adEventType);
-  onBannerAdEvent(AdEventType adEventType);
-  onRewardAdEvent(AdEventType adEventType);
-}
--- lib/src/admob/admob_kit.dart
+++ lib/src/admob/admob_kit.dart
@@ -1,54 +1,65 @@
 import 'dart:async';
 import 'dart:developer';
-import 'package:base_kits/base_kits.dart';
-import 'package:easy_ads_flutter/easy_ads_flutter.dart';
-import 'package:flutter/foundation.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import '../../base_kits.dart';
 
-class AdmobKit extends IAdIdManager {
+class AdmobKit {
   static final AdmobKit _instance = AdmobKit._internal();
   AdmobKit._internal();
   factory AdmobKit() => _instance;
 
   late AdConfig _adConfig;
-  EasyAdBase? bannerAd;
-  StreamSubscription? _openAdEventSub;
+  InterstitialAd? _interstitialAd;
+  AppOpenAd? _appOpenAd;
 
-  EasyBannerAd createBannerAd() {
-    return const EasyBannerAd(
-        adNetwork: AdNetwork.admob, adSize: AdSize.fullBanner);
-  }
-
-  Future<void> preloadBannerAd() async {
-    bannerAd = EasyAds.instance
-        .createBanner(adNetwork: AdNetwork.admob, adSize: AdSize.banner);
-    await bannerAd?.load();
-  }
-
-  Future<void> showInterstitialAd({
-    VoidCallback? readyToShow,
-    VoidCallback? reachLimitation,
-  }) async {
+  Future<void> _loadIntersitial() async {
+    // InterstitialAd
+    final completer = Completer();
     try {
-      AdsCountingManager().checkShouldShowAds(
-        onShouldShowAds: (shouldShowAds) {
-          if (shouldShowAds) {
-            readyToShow != null ? readyToShow() : null;
-            EasyAds.instance.showAd(AdUnitType.interstitial);
-          } else {
-            reachLimitation != null ? reachLimitation() : null;
-          }
-        },
-      );
+      await InterstitialAd.load(
+          adUnitId: _adConfig.interstitialId,
+          request: const AdRequest(),
+          adLoadCallback: InterstitialAdLoadCallback(
+            onAdLoaded: (InterstitialAd ad) {
+              // Keep a reference to the ad so you can show it later.
+              _interstitialAd = ad;
+              completer.complete();
+            },
+            onAdFailedToLoad: (LoadAdError error) {
+              log('InterstitialAd failed to load: $error');
+              completer.complete();
+            },
+          ));
+      await completer.future;
     } catch (e) {
       log(e.toString());
+      completer.complete();
     }
   }
 
-  Future<void> showRewardAds() async {
+  Future<void> _loadOpenAds() async {
+    final completer = Completer();
     try {
-      EasyAds.instance.showAd(AdUnitType.rewarded);
+      await AppOpenAd.load(
+        adUnitId: _adConfig.appOpenId,
+        request: const AdRequest(),
+        adLoadCallback: AppOpenAdLoadCallback(
+          onAdLoaded: (ad) {
+            _appOpenAd = ad;
+            completer.complete();
+          },
+          onAdFailedToLoad: (LoadAdError error) {
+            log('OpenAds failed to load: $error');
+            completer.complete();
+          },
+        ),
+        orientation: AppOpenAd.orientationPortrait,
+      );
+      await completer.future;
     } catch (e) {
       log(e.toString());
+      completer.complete();
     }
   }
 
@@ -60,87 +71,91 @@ class AdmobKit extends IAdIdManager {
     }
   }
 
-  bool isInitialize = false;
-
-  init({
-    required AdConfig adConfig,
-    AdmobEventListener? admobEventListener,
-  }) async {
-    try {
-      _adConfig = adConfig;
-      if (!isInitialize) {
-        await EasyAds.instance.initialize(
-          this,
-          adMobAdRequest: const AdRequest(),
-          fbTestMode: kDebugMode,
-        );
-        EasyAds.instance.onEvent.listen((event) {
-          switch (event.adUnitType) {
-            case AdUnitType.appOpen:
-              admobEventListener?.onOpenAdEvent(event.type);
-              break;
-            case AdUnitType.banner:
-              admobEventListener?.onBannerAdEvent(event.type);
-              break;
-            case AdUnitType.interstitial:
-              admobEventListener?.onInterstitialAdEvent(event.type);
-              break;
-            case AdUnitType.rewarded:
-              admobEventListener?.onRewardAdEvent(event.type);
-              break;
-            default:
-          }
-        });
-      }
-      await preloadBannerAd();
-      isInitialize = true;
-    } catch (e) {
-      log(e.toString(), name: "AdmobKit");
-    }
+  init(AdConfig adConfig) async {
+    _adConfig = adConfig;
+    await MobileAds.instance.initialize();
+    await _loadOpenAds();
+    log('Completed initializing', name: 'AdmobService');
   }
 
-  showAppOpenAd({Function(bool couldShow)? onComplete}) {
-    if (_openAdEventSub != null) {
-      onComplete != null ? onComplete(false) : null;
-      return;
+  Future<void> showInterstitialAd({Function(bool didShow)? onComplete}) async {
+    log('showInterstitialAd', name: 'AdmobService');
+    if (_interstitialAd == null) {
+      await _loadIntersitial();
     }
-    try {
-      log('showAppOpenAd', name: 'AdmobKit');
-      AnalyticKit().logEvent(name: AnalyticEvent.showOpenAds);
-      EasyAds.instance.showAd(AdUnitType.appOpen, delayInSeconds: 0);
-      _openAdEventSub = EasyAds.instance.onEvent.listen((event) {
-        if (event.adUnitType == AdUnitType.appOpen) {
-          _openAdEventSub?.cancel();
-          _openAdEventSub == null;
-          switch (event.type) {
-            case AdEventType.adDismissed:
-              onComplete != null ? onComplete(true) : null;
-              break;
-            default:
-              onComplete != null ? onComplete(false) : null;
-              break;
-          }
-        }
-      });
-    } catch (e) {
-      log(e.toString(), name: "AdmobKit");
-    }
+    _interstitialAd?.fullScreenContentCallback = FullScreenContentCallback(
+      onAdDismissedFullScreenContent: (InterstitialAd ad) {
+        onComplete != null ? onComplete(true) : null;
+        _interstitialAd?.dispose();
+        _interstitialAd = null;
+      },
+      onAdFailedToShowFullScreenContent: (InterstitialAd ad, AdError error) {
+        onComplete != null ? onComplete(false) : null;
+        _interstitialAd?.dispose();
+        _interstitialAd = null;
+      },
+    );
+    await _interstitialAd?.show();
+    AnalyticKit().logEvent(name: AnalyticEvent.showInterstitial);
   }
 
-  @override
-  AppAdIds? get admobAdIds => AppAdIds(
-        appId: _adConfig.adId,
-        appOpenId: _adConfig.appOpenId,
-        bannerId: _adConfig.bannerId,
-        interstitialId: _adConfig.interstitialId,
+  Future<void> showAppOpenAd({Function(bool didShow)? onComplete}) async {
+    log('showAppOpenAd', name: 'AdmobService');
+    if (_appOpenAd == null) {
+      await _loadOpenAds();
+    }
+    _appOpenAd?.fullScreenContentCallback = FullScreenContentCallback(
+      onAdDismissedFullScreenContent: (AppOpenAd ad) {
+        onComplete != null ? onComplete(true) : null;
+        _appOpenAd?.dispose();
+        _appOpenAd = null;
+      },
+      onAdFailedToShowFullScreenContent: (AppOpenAd ad, AdError error) {
+        onComplete != null ? onComplete(false) : null;
+        _appOpenAd?.dispose();
+        _appOpenAd = null;
+      },
+    );
+    await _appOpenAd?.show();
+    AnalyticKit().logEvent(name: AnalyticEvent.showOpenAds);
+  }
+
+  Future<void> forceShowAppOpenAds() async {
+    try {
+      await AppOpenAd.load(
+        adUnitId: _adConfig.appOpenId,
+        request: const AdRequest(),
+        adLoadCallback: AppOpenAdLoadCallback(
+          onAdLoaded: (ad) {
+            ad.show();
+          },
+          onAdFailedToLoad: (LoadAdError error) {
+            log('OpenAds failed to load: $error');
+          },
+        ),
+        orientation: AppOpenAd.orientationPortrait,
       );
+    } catch (e, s) {
+      FirebaseCrashlytics.instance.recordError(e, s);
+    }
+  }
 
-  @override
-  AppAdIds? get appLovinAdIds => null;
-
-  @override
-  AppAdIds? get fbAdIds => null;
-
-  @override
-  AppAdIds? get unityAdIds => null;
+  Future<void> forceShowInterstitialAds() async {
+    try {
+      await InterstitialAd.load(
+          adUnitId: _adConfig.interstitialId,
+          request: const AdRequest(),
+          adLoadCallback: InterstitialAdLoadCallback(
+            onAdLoaded: (InterstitialAd ad) {
+              ad.show();
+            },
+            onAdFailedToLoad: (LoadAdError error) {
+              log('InterstitialAd failed to load: $error');
+            },
+          ));
+    } catch (e, s) {
+      log(e.toString());
+      FirebaseCrashlytics.instance.recordError(e, s);
+    }
+  }
 }

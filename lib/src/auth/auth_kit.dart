6eb40d7b4d3041089b03c2651b3a8e97a4e1a8f1
hmduc1603 <hmduc1603@gmail.com>
2024-08-24 13:32:51 +0100

    Add getAnnonymousUserIdTokenWithAutoExpiration

--- lib/src/auth/auth_kit.dart
+++ lib/src/auth/auth_kit.dart
@@ -21,4 +21,23 @@ class AuthKit {
   Future<void> revokeIdToken() {
     return FirebaseAuth.instance.signOut();
   }
+
+  Future<String> getAnnonymousUserIdTokenWithAutoExpiration(
+      {Duration? expirationDuration}) async {
+    final user = FirebaseAuth.instance.currentUser ??
+        (await FirebaseAuth.instance.signInAnonymously()).user;
+    final token = await user?.getIdToken();
+    if (token != null) {
+      log(token, name: "AuthKit");
+      Future.delayed(expirationDuration ?? const Duration(minutes: 5))
+          .whenComplete(() {
+        FirebaseAuth.instance.signOut().catchError((e) {
+          log(e, name: "AuthKit");
+        });
+      });
+      return token;
+    } else {
+      throw Exception("Failed to get user id token!");
+    }
+  }
 }
